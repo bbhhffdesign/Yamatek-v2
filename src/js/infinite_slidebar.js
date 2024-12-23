e36a755b5cdb57e9746d0985793efd89200a6b34
bauhoffer mariano <bauhoffermariano@gmail.com>
2024-12-23 14:39:15 -0300

    a

--- src/js/infinite_slidebar.js
+++ src/js/infinite_slidebar.js
@@ -1,3 +1,8 @@
-gsap.to(".slide1", {x: "-100%", delay: 1, duration: 10, repeat: -1, ease: "none"})
-gsap.to(".slide2", {x: "-100%", delay: 1, duration: 10, repeat: -1, ease: "none"})
-gsap.to(".slide3", {x: "-100%", delay: 1, duration: 10, repeat: -1, ease: "none"})
+let slideWideth = document.querySelector(".slide1").offsetWidth;
+
+let duration = innerWidth < 1280 ? 10 : 8; 
+
+gsap.to(".slide1", {x: -slideWideth, duration: duration, repeat: -1, ease: "none"})
+gsap.to(".slide2", {x: -slideWideth, duration: duration, repeat: -1, ease: "none"})
+gsap.to(".slide3", {x: -slideWideth, duration: duration, repeat: -1, ease: "none"})
+
