2c8573786a59bfb8325b0204d78fa91fa3e0cd10
bbhhffdesign <bbhhff.design@gmail.com>
2024-12-23 03:58:23 -0300

    a

---
+++ src/js/infinite_slidebar.js
@@ -0,0 +1,3 @@
+gsap.to(".slide1", {x: "-100%", delay: 1, duration: 10, repeat: -1, ease: "none"})
+gsap.to(".slide2", {x: "-100%", delay: 1, duration: 10, repeat: -1, ease: "none"})
+gsap.to(".slide3", {x: "-100%", delay: 1, duration: 10, repeat: -1, ease: "none"})
