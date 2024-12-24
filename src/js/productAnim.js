49be94eb4a66ba12d833f322efb6fd9a3451906f
bauhoffer mariano <bauhoffermariano@gmail.com>
2024-12-24 17:58:03 -0300

    a

--- src/js/productAnim.js
+++ src/js/productAnim.js
@@ -33,8 +33,8 @@ function updateImage(index) {
 }
 
 nextButton.addEventListener("click", (event) => {
-    event.preventDefault();
-    event.stopPropagation();
+    // event.preventDefault();
+    // event.stopPropagation();
     if (currentIndex < productImgs.length - 1) {
         
         gsap.to(productImage,{ 
@@ -59,8 +59,8 @@ nextButton.addEventListener("click", (event) => {
 });
 
 backButton.addEventListener("click", (event) => {
-    event.preventDefault();
-    event.stopPropagation();
+    // event.preventDefault();
+    // event.stopPropagation();
     if (currentIndex > 0) {
 
         gsap.to(productImage,{
